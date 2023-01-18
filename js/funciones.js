f3ce48efb2a245ee44b1b82d8980a198a425547f
emanuel-benjumea <emanuelema2013rapido@gmail.com>
2023-01-18 17:25:13 -0500

    slider de imagenes
    se creo con exito el slider de imagenes solo falta estiliar para acomoda conforme al diseño

--- js/funciones.js
+++ js/funciones.js
@@ -23,6 +23,19 @@ function listo()
     })
 }
 
+
+//slider de imagenes
+
+var counter =1;
+        setInterval(function(){
+        document.getElementById('radio'+counter).checked = true;
+        counter++;
+        if(counter > 4){
+          counter=1;
+        }
+        }, 5000);
+
+
 //funcion dise;o apartamentos
 
 const mostrar = document.getElementById("ocultar");
